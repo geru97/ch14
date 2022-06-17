4d37e12454fe072e2ffe4c53476710d45f005688
Gerardo Ruiz <ruiz.gerardo.r@gmail.com>
2022-06-17 11:26:40 -0500

    proyecto saludo

---
+++ OneDrive/Documentos/Generation/Gerardo/carpetas.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#El simbolo de gato es para hacer comentarios
+
+echo "Te estoy checando hacker Ruso"
+
+mkdir  exercises
+mkdir exercises/cs
+mkdir exercises/cs/cs01
+mkdir exercises/cs/cs02
+
+echo "El usuario que creo estas carpetas es"
+whoami
+
+
